87da295a89d37bc8da44a1ed5287451194523d4f
Fahdlabba <74735730+Fahdlabba@users.noreply.github.com>
2020-12-23 22:19:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function validate(){
+    var ge=document.getElementById("ip");
+    if(ge.value==""){
+        alert("Enter Votre Date");
+        return false
+    }
+    alert("Thank's for ur Message");
+    return true
+
+}
